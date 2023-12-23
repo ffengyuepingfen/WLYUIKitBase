61791d57151ce2d46cf9a85e0db3e362839e6502
wangxiangbo <laoyouhub@icloud.com>
2023-12-23 12:21:04 +0800

    更新 SPM

--- Package.swift
+++ Package.swift
@@ -17,16 +17,14 @@ let package = Package(
             targets: ["WLYUIKitBase"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-//        .package(path: "../WLYUIKitBaseOC")
+        .package(url: "https://gitee.com/laoyouHome/TZImagePicker.git", .upToNextMajor(from: "3.8.5")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "WLYUIKitBase",
-            dependencies: ["OC"],
+            dependencies: ["OC","TZImagePicker"],
             resources: [
                 .process("Resources")
             ]),
